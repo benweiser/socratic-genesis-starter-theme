3a421adabb3aa88c9fee345e872d8947ce32e071
Ben Weiser <hi@benweiser.com>
2015-09-08 12:24:52 -0700

    Added proper doc blocks for comments

--- functions.php
+++ functions.php
@@ -12,24 +12,36 @@
  */
 
 
-// Initialize
+/*
+* Initialize
+*/ 
 require_once (get_stylesheet_directory() . '/lib/init.php');
 
 add_action('genesis_setup', 'bw_theme_setup', 15);
 
-//Theme Set Up Function
+/*
+* Theme Set Up Function
+*/
 function bw_theme_setup() {
       
-    //Custom Image Sizes
+    /*
+    * Custom Image Sizes
+    */
     add_image_size('featured-image', 225, 160, TRUE);
     
-    // Enable Custom Background
+    /*
+    * Enable custom background
+    */
     add_theme_support('custom-background');
     
-    //* Add HTML5 markup structure
+    /* 
+    * Add HTML5 markup structure
+    */
     add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
     
-    // Add support for structural wraps
+    /* 
+     * Add support for structural wraps
+     */
     add_theme_support('genesis-structural-wraps', array(
     'header',
     'subnav',
@@ -40,7 +52,9 @@ function bw_theme_setup() {
     remove_action('genesis_after_header', 'genesis_do_nav');
     add_action('genesis_header_right', 'genesis_do_nav');
     
-    /* Remove uneeded layouts */
+    /* 
+    * Remove uneeded layouts 
+    */
     genesis_unregister_layout('content-sidebar-sidebar');
     genesis_unregister_layout('sidebar-sidebar-content');
     genesis_unregister_layout('sidebar-content-sidebar');
@@ -61,27 +75,37 @@ function bw_theme_setup() {
     	'secondary' => __('Secondary Navigation Menu', CHILD_DOMAIN),
     	));
     
-    //Enqueue Scripts
+    /*
+    * Enqueue Scripts
+    */
     add_action('wp_enqueue_scripts', 'bw_enqueue_scripts');
     
     
-    // Register Sidebars
+    /*
+     * Register Sidebars
+     */ 
     bw_register_sidebars();
     
-    // Unregister SuperFish - Won't be needed in 2.0
+    /*
+     * Unregister SuperFish - Won't be needed in 2.0
+     */ 
+    
     add_action('wp_enqueue_scripts', 'bw_unregister_superfish');
     function bw_unregister_superfish() {
         wp_deregister_script('superfish');
         wp_deregister_script('superfish-args');
     }
-}
+} // End theme setup
 
+/*
+* Add Optional Top Bar Widget Area
+*/
 add_action('genesis_before', 'bw_top_bar');
 function bw_top_bar() {
     genesis_widget_area('top-bar', array('before' => '<div class="top-bar widget-area"><div class="wrap">', 'after' => '</div></div>',));
 }
 
-/**
+/*
  * Register Sidebars
  */
 function bw_register_sidebars() {
@@ -149,8 +173,8 @@ add_theme_support('genesis-accessibility',
 		'rems'
 		));
 
-/**
- * Reponsive Viewport
- */
+/*
+* Reponsive Viewport
+*/
 
 add_theme_support( 'genesis-responsive-viewport' );
